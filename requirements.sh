5dcf92aa819b4b7831de1cf9c01ccb1727ae9f57
Peter c0de <pete_coder@hotmail.com>
2025-02-28 05:02:45 +0200

    Modified: requirements.sh Added   : FFmpeg Desc	: a suite of libraries and programs for handling video, audio, and other multimedia files and streams.

--- requirements.sh
+++ requirements.sh
@@ -11,4 +11,9 @@ python3 -m pip install -U yt-dlp
 # Update:
 pip3 install --upgrade yt-dlp 
 
+# FFmpeg
+# a suite of libraries and programs for handling video, audio, and other multimedia files and streams.
+sudo apt-get install ffmpeg
+
+
 
